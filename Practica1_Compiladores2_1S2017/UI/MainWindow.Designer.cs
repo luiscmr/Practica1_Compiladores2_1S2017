bea96652aa8c3afe38758839e31d8d400b464617
luiscmr <luiiscmr@gmail.com>
2017-02-08 00:28:43 -0600

    Inicio de la interfaz grafica hasta el momento se pueden crear pestañas y eliminarlas abrir, guardar y guardar como

--- Practica1_Compiladores2_1S2017/UI/MainWindow.Designer.cs
+++ Practica1_Compiladores2_1S2017/UI/MainWindow.Designer.cs
@@ -28,11 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.bAdd = new System.Windows.Forms.Button();
+            this.bOpen = new System.Windows.Forms.Button();
+            this.bSave = new System.Windows.Forms.Button();
+            this.bSaveAs = new System.Windows.Forms.Button();
+            this.bClose = new System.Windows.Forms.Button();
+            this.bExec = new System.Windows.Forms.Button();
+            this.bReport = new System.Windows.Forms.Button();
+            this.bAlbum = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bAdd
+            // 
+            this.bAdd.Image = ((System.Drawing.Image)(resources.GetObject("bAdd.Image")));
+            this.bAdd.Location = new System.Drawing.Point(13, 12);
+            this.bAdd.Name = "bAdd";
+            this.bAdd.Size = new System.Drawing.Size(55, 55);
+            this.bAdd.TabIndex = 0;
+            this.bAdd.UseVisualStyleBackColor = true;
+            this.bAdd.Click += new System.EventHandler(this.bAdd_Click);
+            // 
+            // bOpen
+            // 
+            this.bOpen.Image = ((System.Drawing.Image)(resources.GetObject("bOpen.Image")));
+            this.bOpen.Location = new System.Drawing.Point(74, 12);
+            this.bOpen.Name = "bOpen";
+            this.bOpen.Size = new System.Drawing.Size(55, 55);
+            this.bOpen.TabIndex = 1;
+            this.bOpen.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.bOpen.UseVisualStyleBackColor = true;
+            this.bOpen.Click += new System.EventHandler(this.bOpen_Click);
+            // 
+            // bSave
+            // 
+            this.bSave.Image = ((System.Drawing.Image)(resources.GetObject("bSave.Image")));
+            this.bSave.Location = new System.Drawing.Point(135, 12);
+            this.bSave.Name = "bSave";
+            this.bSave.Size = new System.Drawing.Size(55, 55);
+            this.bSave.TabIndex = 2;
+            this.bSave.UseVisualStyleBackColor = true;
+            this.bSave.Click += new System.EventHandler(this.bSave_Click);
+            // 
+            // bSaveAs
+            // 
+            this.bSaveAs.Image = ((System.Drawing.Image)(resources.GetObject("bSaveAs.Image")));
+            this.bSaveAs.Location = new System.Drawing.Point(196, 12);
+            this.bSaveAs.Name = "bSaveAs";
+            this.bSaveAs.Size = new System.Drawing.Size(55, 55);
+            this.bSaveAs.TabIndex = 3;
+            this.bSaveAs.UseVisualStyleBackColor = true;
+            this.bSaveAs.Click += new System.EventHandler(this.bSaveAs_Click);
+            // 
+            // bClose
+            // 
+            this.bClose.Image = ((System.Drawing.Image)(resources.GetObject("bClose.Image")));
+            this.bClose.Location = new System.Drawing.Point(257, 12);
+            this.bClose.Name = "bClose";
+            this.bClose.Size = new System.Drawing.Size(55, 55);
+            this.bClose.TabIndex = 4;
+            this.bClose.UseVisualStyleBackColor = true;
+            this.bClose.Click += new System.EventHandler(this.bClose_Click);
+            // 
+            // bExec
+            // 
+            this.bExec.Image = ((System.Drawing.Image)(resources.GetObject("bExec.Image")));
+            this.bExec.Location = new System.Drawing.Point(318, 12);
+            this.bExec.Name = "bExec";
+            this.bExec.Size = new System.Drawing.Size(55, 55);
+            this.bExec.TabIndex = 5;
+            this.bExec.UseVisualStyleBackColor = true;
+            // 
+            // bReport
+            // 
+            this.bReport.Image = ((System.Drawing.Image)(resources.GetObject("bReport.Image")));
+            this.bReport.Location = new System.Drawing.Point(379, 12);
+            this.bReport.Name = "bReport";
+            this.bReport.Size = new System.Drawing.Size(55, 55);
+            this.bReport.TabIndex = 6;
+            this.bReport.UseVisualStyleBackColor = true;
+            // 
+            // bAlbum
+            // 
+            this.bAlbum.Image = ((System.Drawing.Image)(resources.GetObject("bAlbum.Image")));
+            this.bAlbum.Location = new System.Drawing.Point(440, 12);
+            this.bAlbum.Name = "bAlbum";
+            this.bAlbum.Size = new System.Drawing.Size(55, 55);
+            this.bAlbum.TabIndex = 7;
+            this.bAlbum.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.tabControl);
+            this.panel1.Location = new System.Drawing.Point(13, 74);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 500);
+            this.panel1.TabIndex = 8;
+            // 
+            // tabControl
+            // 
+            this.tabControl.Location = new System.Drawing.Point(4, 4);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(791, 491);
+            this.tabControl.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(820, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(530, 562);
+            this.panel2.TabIndex = 9;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Location = new System.Drawing.Point(12, 580);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1338, 149);
+            this.panel3.TabIndex = 10;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "MainWindow";
+            this.ClientSize = new System.Drawing.Size(1362, 741);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.bAlbum);
+            this.Controls.Add(this.bReport);
+            this.Controls.Add(this.bExec);
+            this.Controls.Add(this.bClose);
+            this.Controls.Add(this.bSaveAs);
+            this.Controls.Add(this.bSave);
+            this.Controls.Add(this.bOpen);
+            this.Controls.Add(this.bAdd);
+            this.Name = "MainWindow";
+            this.Text = "Practica 1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button bAdd;
+        private System.Windows.Forms.Button bOpen;
+        private System.Windows.Forms.Button bSave;
+        private System.Windows.Forms.Button bSaveAs;
+        private System.Windows.Forms.Button bClose;
+        private System.Windows.Forms.Button bExec;
+        private System.Windows.Forms.Button bReport;
+        private System.Windows.Forms.Button bAlbum;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
     }
 }
--- Practica1_Compiladores2_1S2017/UI/MainWindow.cs
+++ Practica1_Compiladores2_1S2017/UI/MainWindow.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,8 +16,151 @@ namespace Practica1_Compiladores2_1S2017.UI
         public MainWindow()
         {
             InitializeComponent();
-            // Maximiza la ventana
             this.WindowState = FormWindowState.Maximized;
         }
+
+        private void bAdd_Click(object sender, EventArgs e)
+        {
+            string title = "Pestaña " + (tabControl.TabPages.Count + 1).ToString();
+            CrearTab(title,"");
+        }
+
+        private void bClose_Click(object sender, EventArgs e)
+        {
+            if (tabControl.TabPages.Count > 0)
+            {
+                tabControl.TabPages.Remove(tabControl.SelectedTab);
+            }
+        }
+
+        private void bOpen_Click(object sender, EventArgs e)
+        {
+            OpenFile();
+
+        }
+
+        private void bSave_Click(object sender, EventArgs e)
+        {
+            SaveFile();
+        }
+
+        private void bSaveAs_Click(object sender, EventArgs e)
+        {
+            SaveFileAs();
+        }
+
+        /*
+            Metodo para Crear Pestañas
+            recibe como parametros el titulo de la pestaña y el Texto 
+        */
+
+        public void CrearTab(String title, String text)
+        {
+            //---------- Nueva Pestaña(TabPage) ----------//
+            TabPage newPage = new TabPage(title);
+            //---------- Text box ----------//
+            TextBox inner_text = new TextBox();
+            inner_text.Name = "texto";
+            inner_text.Text = text;
+            inner_text.Multiline = true;
+            inner_text.ScrollBars = ScrollBars.Vertical;
+            inner_text.SetBounds(0, 0, tabControl.Width - 10, tabControl.Height - 28);
+
+            //---------- Agregar text box a la nueva pestaña ----------//
+            newPage.Controls.Add(inner_text);
+            //---------- Agrega pestaña al tabcontrol ----------//
+            tabControl.TabPages.Add(newPage);
+            tabControl.SelectedTab = newPage;
+            tabControl.ResetText();
+
+        }
+
+
+        /*
+            Metodos para Administrar Archivos
+                 Crear
+                 Abrir
+                 Guardar
+                 Guardar Como
+        */
+
+       
+       //----------- Abrir Archivo ----------//
+
+        public void OpenFile() {
+            OpenFileDialog OFD1 = new OpenFileDialog();
+            OFD1.Filter = "SBScript Files | *.sbs";
+            OFD1.Title = "Seleccione un archivo";
+            if (OFD1.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader str = new StreamReader(OFD1.FileName);
+                CrearTab(OFD1.FileName, str.ReadToEnd());
+            }
+        }
+
+        //---------- Guardar Archivo ------------//
+
+        public void SaveFile() {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "SBScript Files | *.sbs";
+            saveFile.Title = "Guardar Archivo";
+
+            String title = tabControl.SelectedTab.Text;
+            string search = "Pestaña";
+            StringComparison comp = StringComparison.InvariantCultureIgnoreCase;
+            if (title.StartsWith(search, comp) == true)
+            {
+                if(saveFile.ShowDialog() == DialogResult.OK)
+                {
+                    title = saveFile.FileName;
+                }
+            }
+
+            String text = "";
+
+            if (tabControl.SelectedTab.Controls.ContainsKey("texto")) {
+                text = tabControl.SelectedTab.Controls["texto"].Text;
+            }
+
+            File.WriteAllText(title, text);
+
+            tabControl.SelectedTab.Text = title;
+            
+        }
+
+        public void SaveFileAs() {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "SBScript Files | *.sbs";
+            saveFile.Title = "Guardar Archivo";
+
+            string title = tabControl.SelectedTab.Text;
+
+            string[] fileName = title.Split('\\');
+
+            saveFile.FileName = fileName[fileName.Length -1 ];
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                title = saveFile.FileName;
+
+                String text = "";
+
+                if (tabControl.SelectedTab.Controls.ContainsKey("texto"))
+                {
+                    text = tabControl.SelectedTab.Controls["texto"].Text;
+                }
+
+                File.WriteAllText(title, text);
+
+                tabControl.SelectedTab.Text = title;
+            }
+            
+        }
+
+
+
+        //---------- Guardar Archivo Como -----------//
+
+
+
     }
 }
