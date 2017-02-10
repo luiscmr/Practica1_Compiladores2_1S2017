973700a5ac2d595d2eb8a64038f6297c4fc0b8cf
luiscmr <luiiscmr@gmail.com>
2017-02-10 00:18:30 -0600

    cambios minimos en la gramatica

--- Practica1_Compiladores2_1S2017/Analizador/Gramatica.cs
+++ Practica1_Compiladores2_1S2017/Analizador/Gramatica.cs
@@ -19,12 +19,12 @@ namespace Practica1_Compiladores2_1S2017.Analizador
             //Expresion regular para ids con terminacion .sbs, cadena y numero
             RegexBasedTerminal idsbs = new RegexBasedTerminal("[a-zA-Z]([a-zA-Z0-9])*.sbs");
             IdentifierTerminal id = new IdentifierTerminal("identificador");
-            StringLiteral cadena = new StringLiteral("cadena", "\"");
+            StringLiteral cadena = new StringLiteral("cadena", "\"",StringOptions.AllowsLineBreak);
             NumberLiteral numero = new NumberLiteral("numero");
             //Expresion regular para comentarios
             CommentTerminal comentarioBloque = new CommentTerminal("comentarioBloque", "#*", "*#");
             CommentTerminal comentarioLinea = new CommentTerminal("comentarioLinea", "#", "\n");
-            //Con esto se omiten los comentarios  la hora de generar el arbol
+            //Con esto se omiten los comentarios
             base.NonGrammarTerminals.Add(comentarioBloque);
             base.NonGrammarTerminals.Add(comentarioLinea);
 
@@ -36,41 +36,93 @@ namespace Practica1_Compiladores2_1S2017.Analizador
             NonTerminal
                 START = new NonTerminal("Start"),
                 HEADER = new NonTerminal("Header"),
-                ELEMENTO = new NonTerminal("Elemento"),
+                LISTA_HEADER = new NonTerminal("Lista Header"),
                 INCLUYE = new NonTerminal("Incluye"),
-                DEFINE_INCERTEZA = new NonTerminal("Define incerteza"),
-                DEFINE_RUTA = new NonTerminal("Define ruta"),
-                BODY = new NonTerminal("Body");
+                DEFINE_INCERTEZA = new NonTerminal("Define Incerteza"),
+                DEFINE_RUTA = new NonTerminal("Define Ruta"),
+                BODY = new NonTerminal("Body"),
+                VARIABLE = new NonTerminal("Variable"),
+                FUNCION = new NonTerminal("Funcion"),
+                LISTA_BODY = new NonTerminal("Lista Body");
 
             #endregion
 
             #region Gramatica
 
+            // START -> HEADER BODY
             START.Rule = HEADER + BODY;
 
-            HEADER.Rule = MakeStarRule(HEADER, ELEMENTO);
+            #region HEADER
 
-            ELEMENTO.Rule = INCLUYE
+
+            /*
+             HEADER -> HEADER ELEMENTO
+                      |ELEMENTO
+                      |null
+                      ;
+             */
+            HEADER.Rule = MakeStarRule(HEADER, LISTA_HEADER);
+
+
+            /*
+             ELEMENTO -> INCLUYE
+                        |DEFINE_INCERTEZA
+                        |DEFINE_RUTA
+                        ;
+             */
+            LISTA_HEADER.Rule = INCLUYE
                 | DEFINE_INCERTEZA
                 | DEFINE_RUTA
                 ;
 
-
+            // INCLUYE -> 'Incluye' idsbs
             INCLUYE.Rule = "Incluye" + idsbs;
 
+            // DEFINE_INCERTEZA -> 'Define' numero
             DEFINE_INCERTEZA.Rule = "Define" + numero;
 
+            // DEFINE_RUTA -> 'Define' cadena
             DEFINE_RUTA.Rule = "Define" + cadena;
 
-            BODY.Rule = id;
+            #endregion
 
+            #region BODY
+
+            /*
+                 BODY -> BODY LISTA_BODY
+                        | LISTA_BODY
+                        | NULL
+                        ;
+                        
+            */
+
+            BODY.Rule = MakePlusRule(BODY, LISTA_BODY);
+            /*
+                LISTA_BODY = VARIABLE
+                            | FUNCION
+                            ; 
+            */
+            LISTA_BODY.Rule = VARIABLE
+                              | FUNCION
+                              ;
+
+            VARIABLE.Rule = "Number" + id 
+                            | "String" + id
+                            | "Bool" + id
+                            ;
+
+            FUNCION.Rule = id;
+
+
+            #endregion
+            
             #endregion
 
             this.Root = START;
 
-            ELEMENTO.ErrorRule = SyntaxError + Eos;
+            LISTA_HEADER.ErrorRule = SyntaxError + Eos;
             MarkPunctuation("Incluye", "Define");
-            MarkTransient(HEADER, BODY,ELEMENTO);
+            MarkTransient(LISTA_HEADER);
             AddToNoReportGroup(Eos);
 
 
